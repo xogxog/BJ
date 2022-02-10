8efb2e8fc510e18b529e34aa433857ae369cfce3
xogxog <nasa0939@naver.com>
2022-02-10 22:39:32 +0900

    220210 update solved problem

--- 골드/15684.py
+++ 골드/15684.py
@@ -1,53 +1,44 @@
+import sys
 # 사다리 조작
 def play():
-    start = 0
-    r,c=0,0 # r,c
-    while c < N :
-        while r < H :
-            if c == 0 : # 가장 왼쪽 사다리인경우
-                if ladder[r][c] == 1 :
-                    c += 1 # 오른쪽으로 이동
-                    r += 1
-                else :
-                    r += 1
-            else :
-                if ladder[r][c-1] :
-                    c -= 1
-                    r +=1
-                elif ladder[r][c] :
-                    c += 1
-                    r += 1
-                else :
-                    r+= 1
-        if start == c :
-            start += 1
-            c =start
-            r=0
-        else :
-            return 0 # 출발지와 같은곳 못갔으므로
-    return 1 # 모두 출발지와 같은곳에 도착한 경우
 
-def ladder_leg(cnt) :
+    for i in range(N) :
+        now = i
+
+        for j in range(H):
+            if ladder[j][now] == 1:
+                now += 1
+            elif now > 0 and ladder[j][now-1] :
+                now -= 1
+
+        if now != i :
+            return False
+    return True
+
+def ladder_leg(cnt,start) :
     global flag,ans
     if cnt >= 4 :
         ans = -1
         return
-    elif flag :
-        return
-    for j in range(H):
+    # elif flag :
+    #     return
+    # 사다리 가로로 다리 놓음
+    for j in range(start,H):
         for k in range(N-1):
             if ladder[j][k] == 0 :
-                if (k>=1 and (ladder[j][k-1] == 0 or ladder[j][k+1] ==0)) or (k==0 and ladder[j][k+1]==0) : # 가장 왼쪽사다리 아닌경우
+                if (k>=1 and (ladder[j][k-1] == 0 or ladder[j][k+1] ==0)) or (k==0 and ladder[j][k+1]==0) :
                     ladder[j][k] = 1
                     check = play()
-                    if check == 0 and flag == 0:
-                        ladder_leg(cnt+1)
-                    elif check == 1 and flag == 0 :
+                    if not check and flag == 0:
+                        ladder_leg(cnt+1, start+1)
+                    elif check and flag == 0 :
                         # print(check, cnt, flag)
                         flag = 1
                         ans = cnt
                         return
                     ladder[j][k] = 0
+                    if flag :
+                        return
 
     return -1 # 모든 경우의수를 봤는데도 불가능한 경우
 
@@ -58,14 +49,14 @@ flag = 0
 cnt = 1
 ans = 0
 for _ in range(M):
-    a,b = map(int,input().split())
+    a,b = map(int,sys.stdin.readline().split())
     ladder[a-1][b-1] = 1 # b번과 b+1을 잇는다
 
 tmp = play()
 if tmp :
     print(0)
 else :
-    ladder_leg(cnt)
+    ladder_leg(cnt,0)
     print(ans)
 
 
