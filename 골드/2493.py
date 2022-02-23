022b69163c180cb5bfa3487dad735898fc3be318
xogxog <nasa0939@naver.com>
2022-02-23 20:38:09 +0900

    220223 update solved problem

--- 골드/2493.py
+++ 골드/2493.py
@@ -1,30 +1,49 @@
 # 1. 순차적으로 스택에 들어옴. 들어올때(?) 첫번째면, ans에 0 넣기
 # 2. 가장 첫번째보다 작으면 들어옴 / 자기 앞에 것 보다 크고 첫번째보다 작거나 같으면 들어옴.
 # or 자신이 첫번째보다
+
+# 8
+# 10 1 3 7 2 5 4 6
 import sys
 n = int(input())
 ls = list(map(int,sys.stdin.readline().split()))
+
+
+
+
+
+zero_point = [0]
 s=0
 ans =[0]
 for i in range(1,n) :
-    if i == s+1 and ls[i] < ls[s] :
+    if i == s+1 and ls[i] < ls[s] : # s에 맞닿는 경우
         ans += [s + 1]
-        # print(111111, s, ans)
-    elif i > s+1 and ls[i-1] < ls[i] <= ls[s]: # s에 맞닿는 경우
+        # print(000000, s, ans)
+    elif i > s+1 and ls[i-1] < ls[i] < ls[s]: # s에 맞닿는 경우
         ans += [s + 1]
         # print(111111, s, ans)
     elif ans[-1] == 0 and ls[i]>ls[i-1] : # 맞닿는 곳이 없는 경우
+        zero_point += [i]
         s = i
         ans += [0]
         # print(22222,s, ans)
     else:
-        if ls[i-1] >= ls[i] : # 자기 바로앞이 자신보다 크거나 같은 경우
+        if ls[i-1] > ls[i] : # 자기 바로앞이 자신보다 크거나 같은 경우
             s = i-1
             ans += [s+1]
+            zero_point += [i-1]
             # print(333333, s, ans)
         else : # 그렇지 않을 경우 i가 가장 높음
             s = i
-            ans += [s+1] # 자기자신이 가장 높으므로 닿는곳 없음 !?
+            for j in range(len(zero_point)-1,-1,-1) :
+                if ls[zero_point[j]] >= ls[i] : # 닿는곳있으면 .. 추가하자..!
+                    ans += [zero_point[j]+1]
+                    zero_point += [i]
+                    break
+            else :
+                ans += [0]
+                zero_point += [i]
             # print(44444, s, ans)
 
 print(' '.join(list(map(str,ans))))
+
--- 실버/1149(짜다맘).py
+++ 실버/1149(짜다맘).py
@@ -1,28 +1,50 @@
-def check(tmp,idx):
-    global ans
-    if idx == N :
-        if tmp < ans :
-            ans = tmp
-        return
+# 이런방법이 ,,
+# 원래 접근은 모든 수를 보는 것이었는데 시간초과가 났다.
+# 이 접근법은 현재 칠할 색깔 보다 전집에선 나머지 두색깔중 더 숫자가 작은걸 색칠하고, 현재 칠할색을 더해서
+# 결국 리스트 마지막엔 최소로 다 칠했을 경우가 3개나오는데 그 중 제일 작은 수를 출력..
 
-    if tmp > ans :
-        return
+import sys
 
-    for i in range(idx,N):
-        for j in range(3):
-            if not visited[i][j] :
-                visited[i][j] = 1
-                color[i] = j
-                check(tmp+ls[i][j],idx+1)
-                visited[i][j] = 0
-                color[i] = 0
+n = int(sys.stdin.readline())
+
+RGB = [list(map(int,input().split())) for _ in range(n)]
+
+for i in range(1,n) :
+    RGB[i][0] = min(RGB[i-1][1], RGB[i-1][2]) + RGB[i][0] # red
+    RGB[i][1] = min(RGB[i-1][0], RGB[i-1][2]) + RGB[i][1] # green
+    RGB[i][2] = min(RGB[i-1][0], RGB[i-1][1]) + RGB[i][2] #blue
+# print(RGB)
+print(min(RGB[n-1]))
 
 
-N = int(input())
-ls=[list(map(int,input().split())) for _ in range(N)]
-visited = [[0]*3 for _ in range(N)]
-color = [-1]*N
-ans = 987654321
-idx=0
-check(0,idx)
-print(ans)
+# def check(tmp,depth, before_color):
+#     global min_cost
+#     if depth == N :
+#         # print(color)
+#         if tmp < min_cost :
+#             min_cost = tmp
+#         return
+#
+#     if tmp >= min_cost :
+#         return
+#
+#     for i in range(0,3):
+#         if before_color != i : # 전에 칠한 색과 다르다면
+#             # color[depth] = i
+#             check(tmp+ls[depth][i], depth+1, i)
+#
+#
+#
+# N = int(input())
+# ls=[list(map(int,input().split())) for _ in range(N)]
+# # visited = [[0]*3 for _ in range(N)]
+# color = [-1]*N
+# min_cost = 987654321
+# depth=1 # 인덱스 기준
+# before_color = 0
+# for i in range(3) :
+#     color[0]= i # 1번 색칠
+#     before_color = i
+#     check(ls[0][i],depth, before_color)
+#
+# print(min_cost)
