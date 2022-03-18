9473429dce10eaf1e1dbf6417c68a17b98d426f1
xogxog <nasa0939@naver.com>
2022-03-18 19:14:29 +0900

    220318 update solved problem

--- 골드/1107(못품).py
+++ 골드/1107(못품).py
@@ -2,11 +2,18 @@
 import sys
 input = sys.stdin.readline
 number = int(input()) # 가고싶은 채널
-b = int(input()) # 부순 리모컨숫자
+b = int(input()) # 부순 리모컨숫자갯수
 
-break_nums = list(map(int,input().split())) # 부순 숫자
-not_break_nums =[]
-for i in range(10):
-    if i not in break_nums :
-        not_break_nums.append(i)
+break_nums = list(input().split()) # 부순 숫자
+ans = abs(100-number) # +or - 버튼만 눌러서 가는 경우
+
+for num in range(10000001):
+    for N in str(num):
+        if N in break_nums :
+            break
+    else :
+        ans = min(ans, len(str(num)) + abs(num - number))
+
+print(ans)
+
 
