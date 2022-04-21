cc71696252c237547b6950f3f121ee87c54ddc58
xogxog <nasa0939@naver.com>
2022-04-21 23:56:09 +0900

    220421 update solved problem

---
+++ 실버/15724.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+
+people = [list(map(int,input().split())) for _ in range(N)]
+
+for _ in range(int(input())) :
+    x1,y1,x2,y2 = map(int,input().split())
+    ans = 0
+    tmp = people[x1-1:x2]
+    for _ in range(x2-x1+1) :
+        ans += sum(tmp[_][y1-1:y2])
+    print(ans)
