1286cca4cfb47897a706113503aadff852131c16
xogxog <nasa0939@naver.com>
2022-04-25 09:09:14 +0900

    update unsolved probelm 15724

--- 실버/15724.py
+++ 실버/15724.py
@@ -9,6 +9,6 @@ for _ in range(int(input())) :
     x1,y1,x2,y2 = map(int,input().split())
     ans = 0
     tmp = people[x1-1:x2]
-    for _ in range(x2-x1+1) :
+    for _ in range(x1-1,x2-x1+1) :
         ans += sum(tmp[_][y1-1:y2])
     print(ans)
