9610fc43667fd4576de39b2437f0ad811090460d
xogxog <nasa0939@naver.com>
2022-05-26 23:49:03 +0900

    220526 update solved problem

---
+++ 브론즈/2480.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+a,b,c = map(int,input().split())
+
+if a==b==c :
+    print(10000 + (a*1000))
+elif a != b and a !=c and b != c :
+    print(max(a,b,c)*100)
+else :
+    if a == b or a == c:
+        print(1000+(a*100))
+    else :
+        print(1000+(b*100))
