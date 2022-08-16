a02c7ebcedd8d017eba5822e312fdcc45fad8b5e
김태현 <nasa0939@naver.com>
2022-08-16 18:25:32 +0900

    20220816 update solved problem

--- 골드/1941_소물난 칠공주.py
+++ 골드/1941_소물난 칠공주.py
@@ -5,34 +5,41 @@ dr = [0,0,1,-1]
 dc = [1,-1,0,0]
 # 이다솜파 4명 이상 -> 'S'
 
-def dfs(r,c,s,y,cnt) :
+def dfs(s,y,ls) :
     global ans
 
     if y>3 :
         return
 
     if s+y == 7 :
-        if s >= 4 :
-            ans += 1
+        ls.sort()
+        ans.add(tuple(ls))
         return
 
-    for i in range(4) :
-        nr = r + dr[i]
-        nc = c + nr[i]
-        if 0<=nr<5 and 0<=nc<5 and not visited[nr][nc] :
-            visited[nr][nc]=1
-            if princess[nr][nc] =="Y" :
-                dfs(nr,nc,s,y+1,cnt +1)
-            else :
-                dfs(nr, nc, s+1, y, cnt + 1)
-            visited[nr][nc] = 0
+    for r,c in ls :
+        for i in range(4) :
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0<=nr<5 and 0<=nc<5 and not visited[nr][nc] :
+                visited[nr][nc]=1
+                if princess[nr][nc] =="Y" :
+                    dfs(s,y+1,ls+[(nr,nc)])
+                else :
+                    dfs(s+1,y,ls+[(nr,nc)])
+                visited[nr][nc] = 0
 
 princess = [input().rstrip() for _ in range(5)]
-ans = 0
+ans = set()
+visited = [[0]*5 for _ in range(5)]
 
-visited = [[0]*5 for i in range(5)]
-visited[0][0] = 1
-if princess[0][0] == 'Y' :
-    dfs(0,0,0,1,1)
-else :
-    dfs(0, 0, 1, 0, 1)
+for i in range(5) :
+    for j in range(5):
+        if not visited[i][j] :
+            visited[i][j] = 1
+            if princess[i][j] == 'Y' :
+                dfs(0,1,[(i,j)])
+            else :
+                dfs(1, 0,[(i,j)])
+            visited[i][j] = 0
+
+print(len(ans))
