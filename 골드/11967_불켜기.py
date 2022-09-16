9dce37cc50da4db428a25cde1ad147ea82ee1ba0
김태현 <nasa0939@naver.com>
2022-09-17 04:46:19 +0900

    20220917 update solved problem

--- 골드/11967_불켜기.py
+++ 골드/11967_불켜기.py
@@ -5,21 +5,24 @@ input = sys.stdin.readline
 drc = [(0,1),(0,-1),(1,0),(-1,0)]
 
 def light(x,y):
-    visited = [[0] * N for _ in range(N)]
+
     q = deque()
     q.append((x,y))
 
     while q :
         r,c = q.popleft()
 
-        if r==0 and c==0 :
-            return True
-
         for i in range(4):
             nr = r+drc[i][0]
             nc = c+drc[i][1]
-            if 0<=nr<N and 0<=nc<N and not visited[nr][nc] and room[nr][nc] :
-                visited[nr][nc] = 1
+
+            if (nr == 0 and nc == 0) or (nr,nc) in light_on : # 출발 지점에서 이어진 곳이라면
+                light_on.add((x, y))
+                visited[x][y] = 1
+                return True
+
+            if 0<=nr<N and 0<=nc<N and visited[nr][nc] and room[nr][nc] :
+                # visited[nr][nc] = 1
                 q.append((nr,nc))
 
     return False
@@ -27,27 +30,25 @@ def light(x,y):
 N,M = map(int,input().split())
 _dic = defaultdict(list)
 room = [[0]*N for _ in range(N)]
+visited = [[0] * N for _ in range(N)]
 light_on = set()
 light_on.add((0,0))
-# visited = [[0]*N for _ in range(N)]
 
 for _ in range(M):
     x,y,a,b = map(int,input().split())
     _dic[(x-1,y-1)].append((a-1,b-1))
 
 room[0][0] = 1
-# visited[0][0] = 1
 q = deque()
 q.append((0,0))
-
+visited[0][0] = 1
 while q :
     r,c = q.popleft()
 
-    for rr,cc in _dic[(r,c)]:
+    for rr,cc in _dic[(r,c)]: # 방문한 방에서 불 다 켜기
         room[rr][cc] = 1
-        light_on.add((rr,cc))
 
-    for rrr,ccc in _dic[(r,c)]:
+    for rrr,ccc in _dic[(r,c)] :
         TorF = light(rrr,ccc)
         if TorF :
             q.append((rrr,ccc)) # 옮겨 다닐 수 있는 위치
---
+++ 골드/16235_나무재태크.py
@@ -0,0 +1,78 @@
+# 삼성문제는 heapq 안나온다. 괜히 쓰지말자..
+# enumerate 자주 사용하자 존나 유용하다.
+import sys
+
+dr = [-1,-1,-1,0,0,1,1,1]
+dc = [-1,0,1,-1,1,-1,0,1]
+
+input = sys.stdin.readline
+N,M,K = map(int,input().split()) # 땅크기, 나무 갯수, 년
+
+A = [[5]*N for _ in range(N)] # 상도 땅
+food = [] # 양분
+for _ in range(N):
+    tmp = list(map(int,input().split()))
+    food.append(tmp)
+
+trees = [[[]for i in range(N)]for j in range(N)]
+
+for k in range(M):
+    x,y,z = map(int,input().split())
+    trees[x-1][y-1].append(z)
+
+# tmp_trees = [[0]*N for s in range(N)]
+for l in range(K):
+    for m in range(N):
+        for n in range(N):
+            # 봄
+            trees[m][n].sort()
+            dead_t = []
+            for idx,now_tree in enumerate(trees[m][n]) :
+                if A[m][n] >= now_tree : # 양분 먹을 수 있으면
+                    trees[m][n][idx] += 1 # 나이 +1
+                    A[m][n] -= now_tree # 양분 - tree 만큼
+                else : # 양분 못먹어서 죽는 나무
+                    dead_t = trees[m][n][idx:]
+                    trees[m][n] = trees[m][n][:idx]
+                    break
+
+            # 여름
+            for q in range(len(dead_t)):
+                A[m][n] += dead_t[q] // 2
+
+            # 겨울
+            A[m][n] += food[m][n]
+    # print(f'===={l}년, 봄여름====')
+    # print('양분')
+    # for _z in range(N):
+    #     print(*A[_z])
+    # print('심겨진 나무')
+    # for _x in range(N):
+    #     print(*trees[_x])
+
+    # 가을
+    for _m in range(N):
+        for _n in range(N):
+            for tree_age in trees[_m][_n]:
+                if tree_age % 5 == 0:
+                    for _i in range(8):
+                        nr = _m + dr[_i]
+                        nc = _n + dc[_i]
+                        if 0 <= nr < N and 0 <= nc < N:  # 인접칸에 나무 생김
+                            trees[nr][nc].append(1)
+
+
+
+    # print(f'===={l}년, 가을 겨울====')
+    # print('양분')
+    # for _z in range(N):
+    #     print(*A[_z])
+    # print('심겨진 나무')
+    # for _x in range(N):
+    #     print(*trees[_x])
+
+ans = 0
+for _p in range(N):
+    for _q in range(N):
+        ans += len(trees[_p][_q])
+print(ans)
