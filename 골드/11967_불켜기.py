e50c7350829ebc995c3c994be16e59aefd92e9d8
김태현 <nasa0939@naver.com>
2022-09-19 00:25:22 +0900

    20220919 update solved problem

--- 골드/11967_불켜기.py
+++ 골드/11967_불켜기.py
@@ -4,53 +4,45 @@ input = sys.stdin.readline
 
 drc = [(0,1),(0,-1),(1,0),(-1,0)]
 
-def light(x,y):
-
-    q = deque()
-    q.append((x,y))
-
-    while q :
-        r,c = q.popleft()
-
-        for i in range(4):
-            nr = r+drc[i][0]
-            nc = c+drc[i][1]
-
-            if (nr == 0 and nc == 0) or (nr,nc) in light_on : # 출발 지점에서 이어진 곳이라면
-                light_on.add((x, y))
-                visited[x][y] = 1
-                return True
-
-            if 0<=nr<N and 0<=nc<N and visited[nr][nc] and room[nr][nc] :
-                # visited[nr][nc] = 1
-                q.append((nr,nc))
-
-    return False
-
 N,M = map(int,input().split())
 _dic = defaultdict(list)
-room = [[0]*N for _ in range(N)]
+
+light = [[0]*N for _ in range(N)]
+move = [[0]*N for _ in range(N)]
 visited = [[0] * N for _ in range(N)]
-light_on = set()
-light_on.add((0,0))
 
 for _ in range(M):
     x,y,a,b = map(int,input().split())
     _dic[(x-1,y-1)].append((a-1,b-1))
 
-room[0][0] = 1
+light[0][0] = 1
+visited[0][0] = 1
+move[0][0] = 1
+
 q = deque()
 q.append((0,0))
-visited[0][0] = 1
+ans = 1
 while q :
     r,c = q.popleft()
 
+    move[r][c] = 1
+
     for rr,cc in _dic[(r,c)]: # 방문한 방에서 불 다 켜기
-        room[rr][cc] = 1
+        if light[rr][cc] == 0 :
+            ans += 1
+            light[rr][cc] = 1
 
-    for rrr,ccc in _dic[(r,c)] :
-        TorF = light(rrr,ccc)
-        if TorF :
-            q.append((rrr,ccc)) # 옮겨 다닐 수 있는 위치
+    for i in range(4): # 현재 위치에서 돌아다닐 수 있는 위치 체크
+        nr = r+drc[i][0]
+        nc = c+drc[i][1]
+        if 0<=nr<N and 0<=nc<N :
+            move[nr][nc] = 1
 
-print(len(light_on))
+    # 전체 돌면서 방문 가능한 곳(불 켤 수 있는 곳) 체크해서 q에 넣어주기
+    for j in range(N):
+        for k in range(N):
+            if light[j][k] and move[j][k] and not visited[j][k]: # 방 불 켜져있고, 움직일 수 있고, 아직 vistied 하지 않았으면
+                visited[j][k] = 1 # 방문처리하고
+                q.append((j,k))
+
+print(ans)
