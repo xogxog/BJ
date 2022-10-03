7e325f0c8ad234cda716ddba4bb7d9c1038b22df
김태현 <nasa0939@naver.com>
2022-10-04 00:26:36 +0900

    20221004 update solved problem:

--- 단계별풀기/DFS와 BFS/2206_못품.py
+++ 단계별풀기/DFS와 BFS/2206_못품.py
@@ -2,39 +2,34 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-drc = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+drc = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 우하좌상 순서
 
-def bfs(r, c, destroy):
-    global min_cnt
 
+def bfs():
     q = deque()
-    q.append((r, c, destroy))
+    visited = set((0, 0, 0))
+    q.append((0, 0, 0, 1))  # r,c,destroy,cnt
     while q:
-        cr, cc, destroy = q.popleft()
-        if cr == n-1 and cc == m-1:
-            break
+
+        r, c, destroy, cnt = q.popleft()
+        if r == n-1 and c == m-1:
+            return cnt
         for i in range(4):
-            mr = cr + drc[i][0]
-            mc = cc + drc[i][1]
-            if mr < 0 or mr >= n or mc < 0 or mc >= m: continue
-            if visited[mr][mc] !=0 and visited[mr][mc] < visited[cr][cc] + 1: continue
-            if maze[mr][mc] == '0':
-                visited[mr][mc] = visited[cr][cc] + 1
-                q.append((mr, mc, destroy))
-            elif destroy == 0:
-                visited[mr][mc] = visited[cr][cc] + 1
-                q.append((mr, mc, 1))
-    # print(f'=======================')
-    # for j in range(n) :
-    #     print(*visited[j])
+            nr = r + drc[i][0]
+            nc = c + drc[i][1]
+
+            if 0 <= nr < n and 0 <= nc < m and (nr, nc, destroy) not in visited:
+                if maze[nr][nc] == '0':  # 길
+                    q.append((nr, nc, destroy, cnt+1))
+                    visited.add((nr, nc, destroy))
+                elif maze[nr][nc] == '1' and destroy == 0:
+                    q.append((nr, nc, 1, cnt+1))
+                    visited.add((nr, nc, 1))
+    return -1
+
+
 n, m = map(int, input().split())
 maze = [input().rstrip() for _ in range(n)]
-# print(maze)
-visited = [[0]*m for _ in range(n)]
-visited[0][0] = 1
 
-bfs(0, 0, 0)
-if visited[n-1][m-1] == 10000 :
-    print(-1)
-else :
-    print(visited[n-1][m-1])
+
+print(bfs())
